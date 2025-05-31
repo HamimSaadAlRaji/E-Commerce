c78b26026d2f905a4576837f1fa2e7cc793e1fcf
HamimSaadAlRaji <hamimsaad.raji@gmail.com>
2025-05-31 14:20:59 +0600

    feat(admin): Add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="text-red-500f">page</div>;
+  return <div className="text-red-500">page</div>;
 };
 
 export default page;
